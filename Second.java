86fb71d8f550e545ef4c7a815b73d41f6b983a8d
Tero Pajula <teropaj@gmail.com>
2018-12-30 10:42:32 +0200

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,12 @@
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("fsjljfls");
+Third numThree =new Third();
+numThree.give();
+
+	}
+
+}
---
+++ Third.java
@@ -0,0 +1,5 @@
+
+public class Third {
+	public void give () {System.out.print("joo");}
+
+}
